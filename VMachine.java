dc0d088affc391ce8794f3033b1118d9aa81a290
jouo <jashua.heredia@hotmail.com>
2020-04-30 21:28:41 -0500

    Small changes

--- VMachine.java
+++ VMachine.java
@@ -1,23 +1,20 @@
-import java.util.Scanner;
-public class VMachine
-{
-    public static void main(String[] args)
-    {
-        Scanner userIn = new Scanner(System.in);
-        
-        Product[] item = new Product[5];
-        item[0] = new Product("Orange juice", "$1.00", 3);
-        item[1] = new Product("Cola", "$1.25", 12);
-        item[2] = new Product("Water", "$1.00", 15);
-        item[3] = new Product("Cranberry Juice", "$1.50", 2);
-        item[4] = new Product("Sports Drink", "$1.50", 5);
-        
-        
-        
-        
-        
+public class VMachine {
+
+    List<Product> productList;
+
+    public VMachine() {
+        this.productList = new Arraylist<>;
+    }
+
+    public void addProduct(Product product) {
         
     }
-    
-    
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
 }
